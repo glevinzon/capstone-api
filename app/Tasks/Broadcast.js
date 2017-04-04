f8a76387cd6cea232f7f275e6daa488190654e79
Glevinzon Dapal <glevinzon@sci.ph>
2017-04-05 00:08:17 +0800

    fcm-node task

--- app/Tasks/Broadcast.js
+++ app/Tasks/Broadcast.js
@@ -1,4 +1,5 @@
 'use strict';
+var FCM = require('fcm-node');
 
 class Broadcast {
 
@@ -7,6 +8,34 @@ class Broadcast {
   }
 
   * handle() {
+    var serverKey = 'AAAA97QjpFE:APA91bFjditA2VHzGPi2Ox25VX9Hq8UkHz58eSpcAJWC1ANyubZZMB-VZoYVi5gqmg_MOMStPdI0rS2Lwezan1F_zLq5kcYA8wIQ6wC9yzJOLk3sXuWW_HBHuHTbKwgdCiuciaEOX5_i'; //put your server key here
+    var fcm = new FCM(serverKey);
+
+    var data='';
+    var message="Hey! you got this notification.";
+    var title="Capstone Notification";
+    var token="com.itp.glevinzon.capstone>"
+
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+        to: token,
+
+        notification: {
+            title: title,
+            body: message,
+            sound: "default",
+            icon: "ic_launcher" //default notification icon
+        },
+
+        data: data
+    };
+
+    fcm.send(message, function(err, response){
+        if (err) {
+            console.log("Something has gone wrong!", err);
+        } else {
+            console.log("Successfully sent with response: ", response);
+        }
+    });
 
   }
 
