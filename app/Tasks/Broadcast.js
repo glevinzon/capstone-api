c89063a8b68c99a7b413bb187e12912b7675ba67
Glevinzon Dapal <glevinzon.zeal@gmail.com>
2017-04-15 21:44:24 +0800

    changed to Daily Report scheduler

--- app/Tasks/Broadcast.js
+++ app/Tasks/Broadcast.js
@@ -1,5 +1,8 @@
 'use strict';
 const EquationOperation = use('App/Operations/EquationOperation')
+const Tag = use('App/Model/Tag')
+const Record = use('App/Model/Record')
+const moment = use('moment')
 
 class Broadcast {
 
@@ -7,15 +10,17 @@ class Broadcast {
   // More docs here: https://github.com/node-schedule/node-schedule#cron-style-scheduling
   static get schedule() {
     // once every minute
-    return '55 * * * *';
+    return '9 * * *';
   }
 
   // This is the function that is called at the defined schedule
   * handle() {
     let eqOp = new EquationOperation
 
-    let title = 'Quarterly Update'
-    let text = 'This is a test.'
+    let tags = yield Tag.query().count('id as tags')
+    let records = yield Record.query().count('id as records')
+    let title = 'Daily Report'
+    let text = `${records[0].records} Records | ${tags[0].tags} Tags as of ${moment().format()} .`
 
     yield eqOp.broadcastNotif({'title': title, 'text': text})
 
