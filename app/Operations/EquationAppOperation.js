e116bbba95eb51242802fb76e7bd5623235ace09
Glevinzon Dapal <glevinzon.zeal@gmail.com>
2017-05-08 20:28:43 +0800

    rollback

--- app/Operations/EquationAppOperation.js
+++ app/Operations/EquationAppOperation.js
@@ -192,7 +192,7 @@ class EquationAppOperation extends Operation {
       let tags = yield Tag.all()
       let records = yield Record.all()
 
-      return {equation: equation, tags: tags, records: records}
+      return equations
     } catch (e) {
       this.addError(HTTPResponse.STATUS_INTERNAL_SERVER_ERROR, e.message)
       return false
